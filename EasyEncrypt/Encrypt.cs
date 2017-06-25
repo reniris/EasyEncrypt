05dceefdc37aa0a3617c68faa40d664705f244b8
reniris <nyoronu@gmail.com>
2017-06-25 15:08:30 +0900

    暗号化キー回りを調整

--- EasyEncrypt/Encrypt.cs
+++ EasyEncrypt/Encrypt.cs
@@ -7,17 +7,34 @@ using System.Linq;
 
 namespace EasyEncrypt
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	class Encrypt
 	{
-		static public void EncryptFile(string out_path, byte[] data)
+		/// <summary>
+		/// ファイルを暗号化する
+		/// </summary>
+		/// <param name="out_path">暗号化するファイルのパス</param>
+		/// <param name="data">暗号化するデータ</param>
+		/// <param name="key">暗号化キー nullの場合は自動生成</param>
+		/// <returns></returns>
+		static public byte[] EncryptFile(string out_path, byte[] data, byte[] key = null)
 		{
 			using (var fs = new FileStream(out_path, FileMode.Create, FileAccess.Write))
 			{
-				EncryptData(fs, data);
+				return EncryptData(fs, data, key);
 			}
 		}
 
-		static private void EncryptData(Stream out_stream, byte[] data)
+		/// <summary>
+		/// データを暗号化してストリームに出力する
+		/// </summary>
+		/// <param name="out_stream">出力ストリーム</param>
+		/// <param name="data">暗号化するデータ</param>
+		/// <param name="key">暗号化キー nullの場合は自動生成</param>
+		/// <returns></returns>
+		static private byte[] EncryptData(Stream out_stream, byte[] data, byte[] key)
 		{
 			using (var aes = Aes.Create())
 			{
@@ -26,33 +43,48 @@ namespace EasyEncrypt
 				aes.Mode = CipherMode.CBC;        // CBC mode
 				aes.Padding = PaddingMode.PKCS7;    // Padding mode is "PKCS7".
 
-				//キーを自動生成
-				aes.GenerateKey();
 				// IV ( Initilization Vector ) は、自動生成する
 				aes.GenerateIV();
+
+				if (key == null)
+					aes.GenerateKey();  //暗号化キーを自動生成
+				else
+					aes.Key = key;
 
 				//Encryption interface.
 				var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
 				using (CryptoStream cse = new CryptoStream(out_stream, encryptor, CryptoStreamMode.Write))
 				{
-					var xor_key = aes.Key.Select((k, i) => (byte)(k ^ aes.IV[i])).ToArray();
-					out_stream.Write(xor_key, 0, 16);
-					out_stream.Write(aes.IV, 0, 16); // 次にIVも埋め込む
+					out_stream.Write(aes.IV, 0, 16); // 先頭にIVを埋め込む
 					cse.Write(data, 0, data.Length);
 					cse.FlushFinalBlock();
+
+					return aes.Key;
 				}
 			}
 		}
 
-		static public byte[] DecryptFile(string path)
+		/// <summary>
+		/// ファイルを複合化する
+		/// </summary>
+		/// <param name="path">複合化するファイルのパス</param>
+		/// <param name="key">暗号化キー</param>
+		/// <returns></returns>
+		static public byte[] DecryptFile(string path, byte[] key)
 		{
 			using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
 			{
-				return DecryptData(fs);
+				return DecryptData(fs, key);
 			}
 		}
 
-		static private byte[] DecryptData(Stream encrypt_stream)
+		/// <summary>
+		/// データを複合化する
+		/// </summary>
+		/// <param name="encrypt_stream">暗号化された入力ストリーム</param>
+		/// <param name="key">暗号化キー</param>
+		/// <returns></returns>
+		static private byte[] DecryptData(Stream encrypt_stream, byte[] key)
 		{
 			using (var aes = Aes.Create())
 			{
@@ -60,17 +92,13 @@ namespace EasyEncrypt
 				aes.KeySize = 128;                // KeySize = 16bytes
 				aes.Mode = CipherMode.CBC;        // CBC mode
 				aes.Padding = PaddingMode.PKCS7;    // Padding mode is "PKCS7".
-
-				// XOR KEY
-				byte[] xor_key = new byte[16];
-				encrypt_stream.Read(xor_key, 0, 16);
 
 				// Initilization Vector
 				byte[] iv = new byte[16];
 				encrypt_stream.Read(iv, 0, 16);
 				aes.IV = iv;
 
-				aes.Key = xor_key.Select((k, i) => (byte)(k ^ aes.IV[i])).ToArray();
+				aes.Key = key;
 
 				//Decryption interface.
 				ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
--- EasyEncrypt/Program.cs
+++ EasyEncrypt/Program.cs
@@ -4,9 +4,9 @@ using System.IO;
 
 namespace EasyEncrypt
 {
-    class Program
-    {
-        static void Main(string[] args)
+	class Program
+	{
+		static void Main(string[] args)
 		{
 			var dir = AppContext.BaseDirectory;
 			var fullpath = Path.Combine(dir, "save.data");
@@ -15,28 +15,36 @@ namespace EasyEncrypt
 			var data = new GameData();
 			data.Money = 6500;
 
-			byte[] bytes = Encode(fullpath,data);
-			Decode(fullpath);
+			var key = Encode(fullpath, data);   //キーを自動生成して暗号化
+
+			var data2 = Decode(fullpath, key);  //複合化
+			data2.Money = 15000;
+
+			var key2 = Encode(fullpath, data2, key);    //さっき生成されたキーで暗号化
+			Decode(fullpath, key);  //複合化
 
 			Console.ReadLine();
 		}
 
-		private static void Decode(string fullpath)
+		private static GameData Decode(string fullpath, byte[] key)
 		{
-			var bytes = Encrypt.DecryptFile(fullpath);
+			var bytes = Encrypt.DecryptFile(fullpath, key);
 
-			var data2 = MessagePackSerializer.Deserialize<GameData>(bytes);
+			var ret = MessagePackSerializer.Deserialize<GameData>(bytes);
 
 			var json = MessagePackSerializer.ToJson(bytes);
 			Console.WriteLine(json);
+
+			return ret;
 		}
 
-		private static byte[] Encode(string fullpath, GameData data)
+		private static byte[] Encode(string fullpath, GameData data, byte[] enc_key = null)
 		{
 			var bytes = MessagePackSerializer.Serialize(data);
 			Console.WriteLine(BitConverter.ToString(bytes));
-			Encrypt.EncryptFile(fullpath, bytes);
-			return bytes;
-		}		
-    }
+			var gen_key = Encrypt.EncryptFile(fullpath, bytes, enc_key);
+
+			return gen_key;
+		}
+	}
 }
